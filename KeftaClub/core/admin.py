f08283bd8de965285eb5595705d6838dc80992d5
Umap R <imad.rachati@gmail.com>
2024-05-11 15:38:57 +0200

    Change Index page and adding comment below post

--- KeftaClub/core/admin.py
+++ KeftaClub/core/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-from .models import Profile, Post, LikePost, FollowersCount
+from .models import Profile, Post, LikePost, FollowersCount, Comment
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Post)
 admin.site.register(LikePost)
 admin.site.register(FollowersCount)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('body', 'post', 'created_on')
+    list_filter = ('created_on',)
+    search_fields = ('body',)
+    #actions = ['approve_comments']
+
+    #def approve_comments(self, request, queryset):
+    #   queryset.update(active=True)
