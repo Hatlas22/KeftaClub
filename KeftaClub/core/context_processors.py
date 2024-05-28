358ddecd503770e36d6a5881129e0511b5d6365b
Imad-R <92092028+Imad-R@users.noreply.github.com>
2024-05-28 03:08:27 +0200

    Thread update and create room and other feature

--- KeftaClub/core/context_processors.py
+++ KeftaClub/core/context_processors.py
@@ -64,7 +64,7 @@ def nav_bar(request):
         key=lambda x: x["date"],
         reverse=True
     )
-    print(user_profile.user)
+    print(notifications[0])
     return {
         'user_profile': user_profile,
         'followers': followers_list[:4],
